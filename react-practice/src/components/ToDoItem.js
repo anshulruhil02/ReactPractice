7fd87b1a041c60523d5d0b81d03ed708ec8463e7
Anshul Ruhil <anshulruhil02@gmail.com>
2024-02-27 14:01:47 -0500

    Hook setups

---
+++ react-practice/src/components/ToDoItem.js
@@ -0,0 +1,1 @@
+// Main component of the to-do list
---
+++ react-practice/src/components/ToDoList.js
@@ -0,0 +1,10 @@
+// Represennts aa single to-do itemI
+import { useState } from "react";
+
+
+function ToDoList(){
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+}
+
+export default ToDoList;
